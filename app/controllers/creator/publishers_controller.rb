b28238cf4ec04b2d770d8d71e8507d779b4de891
trentonsnyder <trenton.m.snyder@gmail.com>
2018-09-02 10:47:21 -0600

    asdf

--- app/controllers/creator/publishers_controller.rb
+++ app/controllers/creator/publishers_controller.rb
@@ -1,6 +1,6 @@
 class Creator::PublishersController < Creator::BaseController
   def show
     @publisher = Company.where(kind: 'publisher').find(params[:id])
-    @videos = Video.all.first ? [Video.all.limit(4) ].flatten : []
+    @videos = Video.all.first ? Video.all.limit(4) : []
   end
 end
